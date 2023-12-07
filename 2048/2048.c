06a65e7ac4dc87d2eecd2acb06d54d4cfc766d1a
charlesganne <125387743+charlesganne@users.noreply.github.com>
2023-12-08 00:15:42 +0100

    Update 2048.c

--- 2048/2048.c
+++ 2048/2048.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "fonctions_generales.h"
 
 int main(void)
 {
-	//demande a l'utilisateur le mode de jeu dans lequel il veut jouer
-	printf("MENU PRINCIPAL\n");
-	printf("dans quel mode de jeu voulez vous jouer :\n");
-	printf("\"n\" pour normal, \"d\" pour duo, \"p\" pour puzzle\n");
-	char reponse;
-	scanf("%c", &reponse);
-	fflush(stdin);
-	if(reponse == 'n')
-		normal();
-	if(reponse == 'd')
-		duo();
-	if(reponse == 'p')
-		printf("hello world\n");
-		//puzzle();
+	// Toute l'exécution de ce programme admet que l'entrée de l'utilisateur est conforme aux consignes 
+	// affichées à l'écran, et toute exécution avec une entrée invalide n'est pas prise en compte dans ce code, 
+	// ce n'est pas son but.
+	bool jouer = true;
+	while(jouer)
+	{
+		// Demande à l'utilisateur le mode de jeu dans lequel il veut jouer
+		printf("MENU PRINCIPAL\n");
+		printf("Dans quel mode de jeu voulez vous jouer :\n");
+		printf("\"n\" pour normal, \"d\" pour duo, \"p\" pour puzzle :\n");
+
+		char ligne[10]; // Contiendra la ligne entrée par l'utilisateur
+		char reponse; // Y sera placé le premier caracter de la ligne (le seul en exécution nominale)
+		fgets(ligne, sizeof(ligne), stdin); // lis la première ligne entée par l'utilisateur, jusqu'au retour à la ligne, le '\n', juste avant le '\0'
+		reponse = ligne[0];
+		if(reponse == 'n')
+			jouer = normal();
+		if(reponse == 'd')
+			jouer = duo();
+		if(reponse == 'p')
+			jouer = puzzle();
+	}
+	printf("Au revoir\n");
 	return 0;
 }
